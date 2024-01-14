cec08f206fe65def8f78fe832544ed2df9dd7551
Murazakii <murazakii09@gmail.com>
2024-01-14 18:47:45 +0100

    Ajout de la detection automatique du fichier wakfu_chat.log

--- eca_helper.gd
+++ eca_helper.gd
@@ -4,8 +4,18 @@ var log_file:FileAccess
 var position_pointeur:int
 
 @onready var bg_color = $Bg_color
+@onready var file_dialog = $FileDialog
 @onready var label_compteur_sept_fetiche = $CenterContainer/HBoxContainer/LabelCompteurSeptFetiche
 var compteur_sept_fetiche:int = 0 : set = _set_compteur_sept_fetiche
+
+@onready var label_compteur_all_in = $CenterContainer/HBoxContainer/LabelCompteurAllIn
+var compteur_all_in:int = 50 : set = _set_compteur_all_in
+
+func _ready():
+	if FileAccess.file_exists("C:/Users/User/AppData/Roaming/zaap/gamesLogs/wakfu/logs/wakfu_chat.log"):
+		_on_file_dialog_file_selected("C:/Users/User/AppData/Roaming/zaap/gamesLogs/wakfu/logs/wakfu_chat.log")
+	else:
+		file_dialog.show()
 
 func _on_file_dialog_file_selected(path):
 	log_file = FileAccess.open(path, FileAccess.READ)
@@ -15,7 +25,8 @@ func _on_file_dialog_file_selected(path):
 		log_file = FileAccess.open(path, FileAccess.READ)
 		log_file.seek(position_pointeur)
 		while log_file.get_position() < log_file.get_length():
-			if log_file.get_line().contains("Yamisoki: Sept fétiche :"):
+			var line = log_file.get_line()
+			if line.contains("Yamisoki: Sept fétiche :"):
 				compteur_sept_fetiche += 1
 		position_pointeur = log_file.get_position()
 		await get_tree().create_timer(0.2).timeout
@@ -30,6 +41,10 @@ func _set_compteur_sept_fetiche(val):
 		val = val % 6
 	update_label_text(str(val))
 	compteur_sept_fetiche = val
+
+func _set_compteur_all_in(val):
+	label_compteur_all_in.text = str(val) + "%"
+	compteur_all_in = val
 
 func update_label_text(new_val:String)->void:
 	var new_text:String
